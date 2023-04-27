3da0ed602c0d77e7f8e79ecd448e72856ea74b6d
Kamil Kirko <kamil.kirko.wro@gmail.com>
2023-04-28 01:03:28 +0200

    add_models_Ingredient_Quantity

--- Fit_website/admin.py
+++ Fit_website/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from Fit_website.models import Ingredient, MealTime, Meal, TimeofDay, IngredientQuantity
 
-# Register your models here.
+
+admin.site.register(Ingredient)
+admin.site.register(MealTime)
+admin.site.register(Meal)
+admin.site.register(TimeofDay)
+admin.site.register(IngredientQuantity)
--- Fit_website/models.py
+++ Fit_website/models.py
@@ -2,14 +2,13 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class Ingredients(models.Model):
+class Ingredient(models.Model):
     name = models.CharField(max_length=64)
     gramme = models.DecimalField(max_digits=5, decimal_places=2, default=0)
     calories = models.DecimalField(max_digits=5, decimal_places=2, default=0)
     carbohydrates = models.DecimalField(max_digits=5, decimal_places=2, default=0)
     protein = models.DecimalField(max_digits=5, decimal_places=2, default=0)
     fat = models.DecimalField(max_digits=5, decimal_places=2, default=0)
-    quantity = models.PositiveIntegerField(default=1)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
@@ -19,12 +18,20 @@ class Ingredients(models.Model):
 class Meal(models.Model):
     name = models.CharField(max_length=64)
     description = models.TextField()
-    ingredient = models.ManyToManyField(Ingredients)
-    quantity = models.PositiveIntegerField(default=1)
+    ingredients = models.ManyToManyField(Ingredient, through='IngredientQuantity')
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
+
+
+class IngredientQuantity(models.Model):
+    meal = models.ForeignKey(Meal, on_delete=models.CASCADE)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return str(self.quantity)
 
 
 class TimeofDay(models.Model):
